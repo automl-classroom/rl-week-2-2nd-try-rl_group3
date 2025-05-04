5af730e1c81f5dc60dde16804bdae09deb82a28e
Clara Schindler <clara.schindler@stud.uni-hannover.de>
2025-05-04 14:45:27 +0200

    creative 1. Lösung !nicht abgeben!

--- rl_exercises/week_2/my_env.py
+++ rl_exercises/week_2/my_env.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 import gymnasium as gym
+import matplotlib.pyplot as plt  # type: ignore[import]
+import numpy as np
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage  # type: ignore[import]
+from rich import print as printr
 
 
 # ------------- TODO: Implement the following environment -------------
@@ -31,7 +35,73 @@ class MyEnv(gym.Env):
 
     def __init__(self):
         """Initializes the observation and action space for the environment."""
-        pass
+        n_states = 2
+        n_actions = 2
+        self.observation_space = gym.spaces.Discrete(n_states)
+        self.action_space = gym.spaces.Discrete(n_actions)
+        self.horizon = 13
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)  # ChatGPT hat geholfen
+        self.state = 0
+        return self.state, {}
+
+    def step(self, action):
+        self.state = action
+
+        reward = action
+
+        terminated = True
+        truncated = False
+
+        return self.state, reward, terminated, truncated, {}
+
+    def get_reward_per_action(self):
+        reward_matrix = np.array([[0, 1], [0, 1]])
+
+        return reward_matrix
+
+    def get_transition_matrix(self):
+        transition_matrix = np.array(
+            [
+                [[1, 0], [0, 1]],  # state 0
+                [[1, 0], [0, 1]],
+            ]
+        )  # state 1
+
+        return transition_matrix
+
+
+# mars rover
+env = MyEnv()
+actions = [0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0]
+
+states = []
+s, info = env.reset()
+states.append(s)
+for i in range(env.horizon):
+    action = actions[i]
+    s_next, r, terminated, truncated, info = env.step(action)
+    printr(
+        f"Step: {i}, state: {s}, action: {action}, next state: {s_next}, "
+        f"reward: {r}, terminated: {terminated}, truncated: {truncated}"
+    )
+    s = s_next
+    states.append(s)
+
+# Plot
+fig, ax = plt.subplots()
+image = plt.imread("figures/alien_1f47d.png")
+image_box = OffsetImage(image, zoom=0.1)
+x = np.arange(0, len(states))
+y = states
+for x0, y0 in zip(x, y):
+    ab = AnnotationBbox(image_box, (x0, y0), frameon=False)
+    ax.add_artist(ab)
+ax.plot(x, y, c="green")
+ax.set_xlabel("Step")
+ax.set_ylabel("State")
+plt.show()
 
 
 class PartialObsWrapper(gym.Wrapper):
